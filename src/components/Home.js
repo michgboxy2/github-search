97996205f4b71728c2d087f41c7386143ab663c4
michgboxy <michgboxy@gmail.com>
2020-06-20 01:10:17 +0100

    user search module complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../Home.css';
 import SeachResult from './SearchResult';
+import axios from 'axios';
 
 
 const Home = (props) => {
+    const [username, setUsername] = useState('');
+    const [result, setResult] = useState({});
+    const {REACT_APP_API_URL, REACT_APP_TOKEN} = process.env;
+
+    const getList = async () => {
+
+        const options = {
+            method: 'GET',
+            headers: { 'content-type': 'application/x-www-form-urlencoded', 'Authorization': 'token '+REACT_APP_TOKEN },
+            // url : `${REACT_APP_API_URL}users/?q=${username}`,
+            url: `https://api.github.com/search/users?q=${username}`
+          };
+
+
+          let data = await axios(options);
+
+          setResult(data.data);
+    }
+
+    const SearchUser = (e) => {
+        if(e.key === "Enter"){
+            console.log('yup');
+            getList();
+        }
+    }
+
     return (
        <div>
             <section className="main-page">
@@ -13,6 +40,8 @@ const Home = (props) => {
                     <input type="text"
                         placeholder="Search Github"
                         className="search-bar-input"
+                        onChange={(e) => setUsername(e.target.value)}
+                        onKeyPress={SearchUser}
                     />
                     <h4>Pull requests</h4>
                     <h4>Issues</h4>
@@ -20,10 +49,11 @@ const Home = (props) => {
                     <h4>Explore</h4>
                 </div>
             </nav>
-            <SeachResult/>
+            <SeachResult SearchResult={result} />
 
             
         </section>
+    
        </div>
         
 
--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
-const SearchResult = () => {
+const SearchResult = ({SearchResult}) => {
+  const {items} = SearchResult;
+
+  const userInfo = () => {
+    if(items) {
+      return items.map((data, i) => {
+        return (
+          <div className="result" key={i}>
+        <div className="user-profile">
+        <div className="user-avatar">
+           <img src={data.avatar_url} alt="user-avatar"/>
+        </div>
+        
+        <div className="user">
+            <div className="username">
+            <h4>{data.login}</h4>
+            <span>{data.login}</span>
+            </div>
+        </div>
+        </div>
+        <button type="button" className="follow-btn">Follow</button>                
+      </div> 
+        )
+      })
+    }
+  }
+
     return (
         <div className="search-content">
             
                <div className="result-header">
                    <div className="count">
-                        <h4>102,343</h4><span>users</span>
+                        <h4>{SearchResult.total_count}</h4><span>users</span>
                    </div>
                     <button type="button" className="follow-button">Sort: Best match</button>
                </div> 
@@ -26,98 +52,7 @@ const SearchResult = () => {
                     </div>
                     <button type="button" className="follow-btn">Follow</button>                
                   </div> 
-
-                  <div className="result">
-                    <div className="user-profile">
-                    <div className="user-avatar">
-                       <img src="https://wificallrecord.s3.amazonaws.com/document_uploads/47714176093329633131.jpeg" alt="user-avatar"/>
-                    </div>
-                    
-                    <div className="user">
-                        <div className="username">
-                        <h4>Michael King</h4>
-                        <span>Michgboxy</span>
-                        </div>
-                        <div className="user-description">
-                        <p>A marathon runner</p>
-                        <h5>Lagos, Nigeria<span>michgboxy@gmail.com</span></h5>
-                        <p>follower count: 200</p>
-                        <i className="far fa-star">200</i>
-
-                        </div>
-                    </div>
-                    </div>
-                    <button type="button" className="follow-btn">Follow</button>                
-                  </div> 
-
-                  <div className="result">
-                    <div className="user-profile">
-                    <div className="user-avatar">
-                       <img src="https://wificallrecord.s3.amazonaws.com/document_uploads/47714176093329633131.jpeg" alt="user-avatar"/>
-                    </div>
-                    
-                    <div className="user">
-                        <div className="username">
-                        <h4>Michael King</h4>
-                        <span>Michgboxy</span>
-                        </div>
-                        <div className="user-description">
-                        <p>A marathon runner</p>
-                        <h5>Lagos, Nigeria<span>michgboxy@gmail.com</span></h5>
-                        <p>follower count: 200</p>
-                        <i className="far fa-star">200</i>
-
-                        </div>
-                    </div>
-                    </div>
-                    <button type="button" className="follow-btn">Follow</button>                
-                  </div> 
-
-                  <div className="result">
-                    <div className="user-profile">
-                    <div className="user-avatar">
-                       <img src="https://wificallrecord.s3.amazonaws.com/document_uploads/47714176093329633131.jpeg" alt="user-avatar"/>
-                    </div>
-                    
-                    <div className="user">
-                        <div className="username">
-                        <h4>Michael King</h4>
-                        <span>Michgboxy</span>
-                        </div>
-                        <div className="user-description">
-                        <p>A marathon runner</p>
-                        <h5>Lagos, Nigeria<span>michgboxy@gmail.com</span></h5>
-                        <p>follower count: 200</p>
-                        <i className="far fa-star">200</i>
-
-                        </div>
-                    </div>
-                    </div>
-                    <button type="button" className="follow-btn">Follow</button>                
-                  </div> 
-
-                  <div className="result">
-                    <div className="user-profile">
-                    <div className="user-avatar">
-                       <img src="https://wificallrecord.s3.amazonaws.com/document_uploads/47714176093329633131.jpeg" alt="user-avatar"/>
-                    </div>
-                    
-                    <div className="user">
-                        <div className="username">
-                        <h4>Michael King</h4>
-                        <span>Michgboxy</span>
-                        </div>
-                        <div className="user-description">
-                        <p>A marathon runner</p>
-                        <h5>Lagos, Nigeria<span>michgboxy@gmail.com</span></h5>
-                        <p>follower count: 200</p>
-                        <i className="far fa-star">200</i>
-
-                        </div>
-                    </div>
-                    </div>
-                    <button type="button" className="follow-btn">Follow</button>                
-                  </div> 
+                  {userInfo()}
                   <footer className="page-footer">
                   <ul>
                     <li>@2020 Github, inc</li>
