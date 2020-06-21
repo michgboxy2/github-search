3b500ba545be1c2c0310078f16624b53881416c9
michgboxy <michgboxy@gmail.com>
2020-06-21 16:39:43 +0100

    add home.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import '../home.css';
+import './home.css';
 import SeachResult from './SearchResult';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
